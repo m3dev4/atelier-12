4da4674b52a02a4063360a8078289c81c309dc42
Mouhamed <m3dev4@gmail.com>
2026-01-08 13:23:09 +0000

    Amelioration de l'affichage & optimisation des differentes fonctions

--- main.py
+++ main.py
@@ -34,18 +34,17 @@ def orange_money_menu():
 
     while True:
         choix = input(
-            """
-                1. Solde de mes comptes\n
-                2. Achats: Crédit et Pass
-                3. Transfert d'argent
-                5. Quitter
-                """
+        """
+        1. Solde de mes comptes
+        2. Achats: Crédit et Pass
+        3. Transfert d'argent
+        5. Quitter
+        choix: """
         )
 
         match choix:
             case "1":
                 my_account()
-                
 
             case "2":
                 buying()
@@ -61,39 +60,95 @@ def orange_money_menu():
                 print("Choix incorrect !")
                 choix = input(
                     """
-                1. Solde de mes comptes\n
+                1. Solde de mes comptes
                 2. Achats: Crédit et Pass
                 3. Transfert d'argent
                 5. Quitter
-                """
+                choix: """
                 )
 
-
+#Fonction du sous menu de solde de mes comptes
 def my_account():
-    print("Solde de mes comptes")
+    print("*********Mon compte:*********")
+    while True:
+        choix = input(
+            """
+            1. Solde du compte
+            2. Mon salaire
+              --- 
+            0. prec
+            9. Accueil
+            choix: """
+        )
 
+        match choix:
+            case "1":
+                solde()
 
+            case "2":
+                print("Mon salaire")
+
+            case "0" | "9":
+                orange_money_menu()
+                break
+
+            case _:
+                print("Choix incorrect !")
+
+#Fonction du sous menu qui concerne l'achat du crédit
 def buying():
-    print("Achats: Crédit et Pass")
+    print("*********Achats: Crédit et Pass*********")
+    while True:
+        choix = input(
+            """
+            1. Crédit
+            2. Pass
+              --- 
+            0. prec
+            9. Accueil
+            choix: """
+        )
 
+        match choix:
+            case "1":
+                credit()
 
+            case "2":
+                print("Pass")
+
+            case "0":
+                my_account()
+                
+            case "9":
+                orange_money_menu()
+                break
+
+            case _:
+                print("Choix incorrect !")
+
+#Fonction du transfert d'argent
 def transfer():
     print("Transfert d'argent")
 
-#Cette fonction a pour role de faire un retour à l'arriere l'endroit ou on etait
-def navigation_back():
-    back = 0
+#Fonction pour afficher le solde 
+def solde():
+    print("*********Solde de mes comptes:*********")
+    my_solde = 100000
+    print(f"Le solde de votre compte est de: {my_solde} FCFA")
     while True:
-        back_interface = input("Précedent 0: ")
+        back_interface = input("0:prec\n 9.Accueil ")
+        # home_interface = input("9: Accueil")
 
         if back_interface == "0":
-            orange_money_menu()
+            my_account()
             break
+        elif back_interface == "9":
+             orange_money_menu()
         else:
             print("Choix incorrect !")
-            
-            
-            
-            
+
+def credit():
+    print("Crédit")
+
 if __name__ == "__main__":
     menuUSSD()
